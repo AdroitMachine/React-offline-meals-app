6e9471bb7eac3d03949c415264216644a3081ef1
AdroitMachine <anujsaxenamail@gmail.com>
2021-05-01 00:03:49 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import Cart from './components/Cart/Cart';
+import Header from './components/Layout/Header'
+import Meals from './components/Meals/Meals'
+import { useState } from 'react'
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+  return (
+    <>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
+}
+
+export default App;
